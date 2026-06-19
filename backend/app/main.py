851a06311434a03c2ee984255b40396cf598ec1a
ziad ahmeddrwish <114486355+ziadelmdadrwish@users.noreply.github.com>
2026-06-19 12:42:48 +0300

    Update main.py
    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,10 +33,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
 )
 
 
